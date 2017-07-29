cedb24626bb464ea88eebc3ff9c46fcf1971417b
hobbyquaker <hq@ccu.io>
2017-07-29 06:28:24 +0200

    make StatusLowBattery=BATTERY_LEVEL_NORMAL default on missing payload

--- accessories/CarbonDioxideSensor.js
+++ accessories/CarbonDioxideSensor.js
@@ -1,4 +1,4 @@
-/* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off" */
+/* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off", no-negated-condition: "off" */
 
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, newAccessory, Service, Characteristic} = iface;
@@ -32,17 +32,17 @@ module.exports = function (iface) {
                 .getCharacteristic(Characteristic.StatusLowBattery)
                 .on('get', callback => {
                     log.debug('< hap get', settings.name, 'StatusLowBattery');
-                    const bat = mqttStatus[settings.topic.statusLowBattery] === settings.payload.onLowBattery ?
-                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
-                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                    const bat = mqttStatus[settings.topic.statusLowBattery] !== settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
                     log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
                     callback(null, bat);
                 });
 
             mqttSub(settings.topic.statusLowBattery, val => {
-                const bat = val === settings.payload.onLowBattery ?
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                const bat = val !== settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
                 log.debug('> hap set', settings.name, 'StatusLowBattery', bat);
                 sensor.getService(Service.CarbonDioxideSensor)
                     .setCharacteristic(Characteristic.StatusLowBattery, bat);
--- accessories/MotionSensor.js
+++ accessories/MotionSensor.js
@@ -1,4 +1,4 @@
-/* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off" */
+/* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off", no-negated-condition: "off" */
 
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, newAccessory, Service, Characteristic} = iface;
@@ -28,18 +28,18 @@ module.exports = function (iface) {
                 .getCharacteristic(Characteristic.StatusLowBattery)
                 .on('get', callback => {
                     log.debug('< hap get', settings.name, 'StatusLowBattery');
-                    const bat = mqttStatus[settings.topic.statusLowBattery] === settings.payload.onLowBattery ?
-                        Characteristic.StatusLowBattery.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
-                        Characteristic.StatusLowBattery.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                    const bat = mqttStatus[settings.topic.statusLowBattery] !== settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                        Characteristic.StatusLowBattery.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
 
                     log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
                     callback(null, bat);
                 });
 
             mqttSub(settings.topic.statusLowBattery, val => {
-                const bat = val === settings.payload.onLowBattery ?
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                const bat = val !== settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
                 log.debug('> hap set', settings.name, 'statusLowBattery', bat);
                 sensor.getService(Service.ContactSensor)
                     .setCharacteristic(Characteristic.StatusLowBattery, bat);
