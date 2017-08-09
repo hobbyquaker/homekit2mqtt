5ffd017edda172fc7d3b615178bb804509139993
hobbyquaker <hq@ccu.io>
2017-08-09 23:21:13 +0200

    add lowBat characteristic and todo

--- accessories/TemperatureSensor.js
+++ accessories/TemperatureSensor.js
@@ -1,5 +1,12 @@
 /* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off" */
 
+/* TODO
+ this.addOptionalCharacteristic(Characteristic.StatusActive);
+ this.addOptionalCharacteristic(Characteristic.StatusFault);
+ this.addOptionalCharacteristic(Characteristic.StatusLowBattery);
+ this.addOptionalCharacteristic(Characteristic.StatusTampered);
+ */
+
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, newAccessory, Service, Characteristic} = iface;
 
@@ -30,6 +37,29 @@ module.exports = function (iface) {
                 log.debug('> hap re_get', settings.name, temperature);
                 callback(null, temperature);
             });
+
+        if (settings.topic.statusLowBattery) {
+            sensor.addService(Service.TemperatureSensor, settings.name)
+                .getCharacteristic(Characteristic.StatusLowBattery)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusLowBattery');
+                    const bat = mqttStatus[settings.topic.statusLowBattery] !== settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+
+                    log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
+                    callback(null, bat);
+                });
+
+            mqttSub(settings.topic.statusLowBattery, val => {
+                const bat = val !== settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+                log.debug('> hap update', settings.name, 'StatusLowBattery', bat);
+                sensor.getService(Service.TemperatureSensor)
+                    .updateCharacteristic(Characteristic.StatusLowBattery, bat);
+            });
+        }
 
         return sensor;
     };
