a660324c1a5f8b69d872856e988179bcf8b2559d
hobbyquaker <hq@ccu.io>
2017-08-10 19:00:13 +0200

    add LowBattery characteristic

--- accessories/HumiditySensor.js
+++ accessories/HumiditySensor.js
@@ -4,7 +4,6 @@
  this.addOptionalCharacteristic(Characteristic.StatusActive);
  this.addOptionalCharacteristic(Characteristic.StatusFault);
  this.addOptionalCharacteristic(Characteristic.StatusTampered);
- this.addOptionalCharacteristic(Characteristic.StatusLowBattery);
  */
 
 module.exports = function (iface) {
@@ -27,6 +26,28 @@ module.exports = function (iface) {
                 callback(null, mqttStatus[settings.topic.statusHumidity]);
             });
 
+        if (settings.topic.statusLowBattery) {
+            sensor.getService(Service.HumiditySensor, settings.name)
+                .getCharacteristic(Characteristic.StatusLowBattery)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusLowBattery');
+                    const bat = mqttStatus[settings.topic.statusLowBattery] === settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+
+                    log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
+                    callback(null, bat);
+                });
+
+            mqttSub(settings.topic.statusLowBattery, val => {
+                const bat = val === settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                log.debug('> hap update', settings.name, 'StatusLowBattery', bat);
+                sensor.getService(Service.HumiditySensor)
+                    .updateCharacteristic(Characteristic.StatusLowBattery, bat);
+            });
+        }
         return sensor;
     };
 };
