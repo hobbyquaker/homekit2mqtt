1daebd394f431f8dcecb96c9f8a2b9b23420d0fe
hobbyquaker <hq@ccu.io>
2017-08-10 19:33:12 +0200

    add LowBattery characteristic

--- accessories/LightSensor.js
+++ accessories/LightSensor.js
@@ -4,7 +4,6 @@
  this.addOptionalCharacteristic(Characteristic.StatusActive);
  this.addOptionalCharacteristic(Characteristic.StatusFault);
  this.addOptionalCharacteristic(Characteristic.StatusTampered);
- this.addOptionalCharacteristic(Characteristic.StatusLowBattery);
  */
 
 module.exports = function (iface) {
@@ -26,6 +25,30 @@ module.exports = function (iface) {
                 log.debug('> hap re_get', settings.name, mqttStatus[settings.topic.statusAmbientLightLevel]);
                 callback(null, mqttStatus[settings.topic.statusAmbientLightLevel]);
             });
+
+        /* istanbul ignore else */
+        if (settings.topic.statusLowBattery) {
+            sensor.getService(Service.LightSensor, settings.name)
+                .getCharacteristic(Characteristic.StatusLowBattery)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusLowBattery');
+                    const bat = mqttStatus[settings.topic.statusLowBattery] === settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+
+                    log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
+                    callback(null, bat);
+                });
+
+            mqttSub(settings.topic.statusLowBattery, val => {
+                const bat = val === settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+                log.debug('> hap update', settings.name, 'StatusLowBattery', bat);
+                sensor.getService(Service.LightSensor)
+                    .updateCharacteristic(Characteristic.StatusLowBattery, bat);
+            });
+        }
 
         return sensor;
     };
