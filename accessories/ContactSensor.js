c8c0a91026ded0fd63fe60df27fa8fe7ad9776e0
Matthew Sheffield <matthew@sheffield.io>
2018-01-06 00:39:18 -0600

    Added StatusActive, StatusFault, and StatusTampered support to contact sensor.

--- accessories/ContactSensor.js
+++ accessories/ContactSensor.js
@@ -34,6 +34,7 @@ module.exports = function (iface) {
         });
 
         /* istanbul ignore else */
+		/* Optional: Low Battery Status */
         if (settings.topic.statusLowBattery) {
             sensor.getService(Service.ContactSensor, settings.name)
                 .getCharacteristic(Characteristic.StatusLowBattery)
@@ -53,6 +54,79 @@ module.exports = function (iface) {
                 log.debug('> hap update', settings.name, 'StatusLowBattery', bat);
                 sensor.getService(Service.ContactSensor)
                     .updateCharacteristic(Characteristic.StatusLowBattery, bat);
+            });
+        }
+        
+        /* istanbul ignore else */
+        /* Optional: Status Tampered */
+        if (settings.topic.statusTampered) {
+            sensor.getService(Service.ContactSensor, settings.name)
+                .getCharacteristic(Characteristic.StatusTampered)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusTampered');
+                    const tampered = mqttStatus[settings.topic.statusTampered] !== settings.payload.onTampered ?
+                        Characteristic.StatusTampered.NOT_TAMPERED :
+                        Characteristic.StatusTampered.TAMPERED;
+                    log.debug('> hap re_get', settings.name, 'StatusTampered', tampered);
+                    callback(null, tampered);
+                });
+
+            mqttSub(settings.topic.statusTampered, val => {
+                const tampered = val !== settings.payload.onStatusTampered ?
+                    Characteristic.StatusTampered.NOT_TAMPERED :
+                    Characteristic.StatusTampered.TAMPERED;
+                log.debug('> hap update', settings.name, 'StatusTampered', tampered);
+                sensor.getService(Service.ContactSensor)
+                    .updateCharacteristic(Characteristic.StatusTampered, tampered);
+            });
+        }
+        
+        /* istanbul ignore else */
+        /* Optional: Status Active */
+        if (settings.topic.statusActive) {
+            sensor.getService(Service.ContactSensor, settings.name)
+                .getCharacteristic(Characteristic.StatusActive)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusActive');
+                    const act = mqttStatus[settings.topic.statusActive] !== settings.payload.onStatusActive ? 0 : 1;
+                        
+                        //Characteristic.Active.INACTIVE :
+                        //Characteristic.Active.ACTIVE
+                    log.debug('> hap re_get', settings.name, 'StatusActive', act);
+                    callback(null, act);
+                });
+
+            mqttSub(settings.topic.statusActive, val => {
+                const act = val !== settings.payload.onStatusActive ? 0 : 1;
+                    //Characteristic.Active.INACTIVE :
+                    //Characteristic.Active.ACTIVE;
+                log.debug('> hap update', settings.name, 'StatusActive', act);
+                sensor.getService(Service.ContactSensor)
+                    .updateCharacteristic(Characteristic.StatusActive, act);
+            });
+        }
+        
+        /* istanbul ignore else */
+        /* Optional: Status Fault */
+        if (settings.topic.statusFault) {
+            sensor.getService(Service.ContactSensor, settings.name)
+                .getCharacteristic(Characteristic.StatusFault)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusFault');
+                    const fault = mqttStatus[settings.topic.statusFault] !== settings.payload.onStatusFault ?
+                        Characteristic.StatusFault.NO_FAULT :
+                        Characteristic.StatusFault.GENERAL_FAULT
+                    log.debug('> hap re_get', settings.name, 'StatusFault', fault);
+                    callback(null, fault);
+                });
+
+            mqttSub(settings.topic.statusFault, val => {
+                const fault = val !== settings.payload.onStatusFault ?
+                    Characteristic.StatusFault.NO_FAULT :
+                    Characteristic.StatusFault.GENERAL_FAULT;
+                log.debug('> hap update', settings.name, 'StatusFault', fault);
+                sensor.getService(Service.ContactSensor)
+                    .updateCharacteristic(Characteristic.StatusFault, fault);
             });
         }
 
