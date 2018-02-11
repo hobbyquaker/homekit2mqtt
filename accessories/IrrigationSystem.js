e417944a16c1e9729bf81452d5dbfe23f3578766
hobbyquaker <bastiraff@googlemail.com>
2018-02-11 15:15:07 +0100

    implement valve and irrigation

--- accessories/IrrigationSystem.js
+++ accessories/IrrigationSystem.js
@@ -3,8 +3,6 @@
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, newAccessory, Service, Characteristic} = iface;
 
-    // TODO Implement
-
     /*
    // Required Characteristics
   this.addCharacteristic(Characteristic.Active);
@@ -17,7 +15,126 @@ module.exports = function (iface) {
   this.addOptionalCharacteristic(Characteristic.StatusFault);
      */
 
+
     return function createAccessory_IrrigationSystem(settings) {
-        throw new Error('Service IrrigationSystem not yet implemented');
+        const irrigation = newAccessory(settings);
+
+        if (typeof settings.payload.activeTrue === 'undefined') {
+            settings.payload.activeTrue = true;
+        }
+
+        if (typeof settings.payload.inUseTrue === 'undefined') {
+            settings.payload.inUseTrue = true;
+        }
+
+        if (typeof settings.payload.faultTrue === 'undefined') {
+            settings.payload.faultTrue = true;
+        }
+
+        if (typeof settings.payload.activeFalse === 'undefined') {
+            settings.payload.activeFalse = false;
+        }
+
+
+        irrigation.addService(Service.IrrigationSystem, settings.name)
+            .getCharacteristic(Characteristic.Active)
+            .on('set', (value, callback) => {
+                log.debug('< hap set', settings.name, 'Active', value);
+                const active = value ? settings.payload.activeTrue : settings.payload.activeFalse;
+                log.debug('> mqtt', settings.topic.setActive, active);
+                mqttPub(settings.topic.setActive, active);
+                callback();
+            });
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusActive) {
+            mqttSub(settings.topic.statusActive, val => {
+                log.debug('< mqtt', settings.topic.statusActive, val);
+                const active = mqttStatus[settings.topic.statusActive] === settings.payload.activeTrue ? 1 : 0;
+                log.debug('> hap update', settings.name, 'Active', active);
+                irrigation.getService(Service.IrrigationSystem)
+                    .updateCharacteristic(Characteristic.Active, active);
+            });
+            irrigation.getService(Service.IrrigationSystem)
+                .getCharacteristic(Characteristic.Active)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'Active');
+                    const active = mqttStatus[settings.topic.statusActive] === settings.payload.activeTrue ? 1 : 0;
+                    log.debug('> hap re_get', settings.name, 'Active', active);
+                    callback(null, active);
+                });
+        }
+
+        mqttSub(settings.topic.statusProgramMode, val => {
+            log.debug('< mqtt', settings.topic.statusProgramMode, val);
+            const mode = mqttStatus[settings.topic.statusProgramMode];
+            log.debug('> hap update', settings.name, 'ProgramMode', mode);
+            irrigation.getService(Service.IrrigationSystem)
+                .updateCharacteristic(Characteristic.ProgramMode, mode);
+        });
+        irrigation.getService(Service.IrrigationSystem)
+            .getCharacteristic(Characteristic.ProgramMode)
+            .on('get', callback => {
+                log.debug('< hap get', settings.name, 'ProgramMode');
+                const mode = mqttStatus[settings.topic.statusProgramMode];
+                log.debug('> hap re_get', settings.name, 'ProgramMode', mode);
+                callback(null, mode);
+            });
+
+        mqttSub(settings.topic.statusInUse, val => {
+            log.debug('< mqtt', settings.topic.statusInUse, val);
+            const inUse = mqttStatus[settings.topic.statusInUse] === settings.payload.inUseTrue ? 1 : 0;
+            log.debug('> hap update', settings.name, 'InUse', inUse);
+            irrigation.getService(Service.IrrigationSystem)
+                .updateCharacteristic(Characteristic.InUse, inUse);
+        });
+        irrigation.getService(Service.IrrigationSystem)
+            .getCharacteristic(Characteristic.InUse)
+            .on('get', callback => {
+                log.debug('< hap get', settings.name, 'InUse');
+                const inUse = mqttStatus[settings.topic.statusInUse] === settings.payload.inUseTrue ? 1 : 0;
+                log.debug('> hap re_get', settings.name, 'InUse', inUse);
+                callback(null, inUse);
+            });
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusRemainingDuration) {
+            mqttSub(settings.topic.statusRemainingDuration, val => {
+                log.debug('< mqtt', settings.topic.statusRemainingDuration, val);
+                const remainingDuration = mqttStatus[settings.topic.statusRemainingDuration];
+                log.debug('> hap update', settings.name, 'RemainingDuration', remainingDuration);
+                irrigation.getService(Service.IrrigationSystem)
+                    .updateCharacteristic(Characteristic.RemainingDuration, remainingDuration);
+            });
+            irrigation.getService(Service.IrrigationSystem)
+                .getCharacteristic(Characteristic.RemainingDuration)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'RemainingDuration');
+                    const inUse = mqttStatus[settings.topic.statusRemainingDuration];
+                    log.debug('> hap re_get', settings.name, 'RemainingDuration', inUse);
+                    callback(null, inUse);
+                });
+        }
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusFault) {
+            mqttSub(settings.topic.statusFault, val => {
+                log.debug('< mqtt', settings.topic.statusFault, val);
+                const fault = mqttStatus[settings.topic.statusFault] === settings.payload.faultTrue ? 1 : 0;
+                log.debug('> hap update', settings.name, 'StatusFault', fault);
+                irrigation.getService(Service.IrrigationSystem)
+                    .updateCharacteristic(Characteristic.StatusFault, fault);
+            });
+            irrigation.getService(Service.IrrigationSystem)
+                .getCharacteristic(Characteristic.StatusFault)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusFault');
+                    const fault = mqttStatus[settings.topic.statusFault] === settings.payload.faultTrue ? 1 : 0;
+                    log.debug('> hap re_get', settings.name, 'StatusFault', fault);
+                    callback(null, fault);
+                });
+        }
+
+        return irrigation;
     };
 };
--- accessories/Valve.js
+++ accessories/Valve.js
@@ -3,8 +3,6 @@
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, newAccessory, Service, Characteristic} = iface;
 
-    // TODO Implement
-
     /*
     Service.Valve = function(displayName, subtype) {
     Service.call(this, displayName, '000000D0-0000-1000-8000-0026BB765291', subtype);
@@ -30,6 +28,128 @@ module.exports = function (iface) {
      */
 
     return function createAccessory_Valve(settings) {
-        throw new Error('Service Valve not yet implemented');
-    };
+        const valve = newAccessory(settings);
+
+        if (typeof settings.payload.activeTrue === 'undefined') {
+            settings.payload.activeTrue = true;
+        }
+
+        if (typeof settings.payload.inUseTrue === 'undefined') {
+            settings.payload.inUseTrue = true;
+        }
+
+        if (typeof settings.payload.faultTrue === 'undefined') {
+            settings.payload.faultTrue = true;
+        }
+
+        if (typeof settings.payload.activeFalse === 'undefined') {
+            settings.payload.activeFalse = false;
+        }
+
+
+        valve.addService(Service.Valve, settings.name)
+            .getCharacteristic(Characteristic.Active)
+            .on('set', (value, callback) => {
+                log.debug('< hap set', settings.name, 'Active', value);
+                const active = value ? settings.payload.activeTrue : settings.payload.activeFalse;
+                log.debug('> mqtt', settings.topic.setActive, active);
+                mqttPub(settings.topic.setActive, active);
+                callback();
+            });
+
+        const type = settings.config.valveType || 0;
+        log.debug('> hap set', settings.name, 'ValveType', type);
+        valve.getService(Service.Valve)
+            .setCharacteristic(Characteristic.ValveType, type);
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusActive) {
+            mqttSub(settings.topic.statusActive, val => {
+                log.debug('< mqtt', settings.topic.statusActive, val);
+                const active = mqttStatus[settings.topic.statusActive] === settings.payload.activeTrue ? 1 : 0;
+                log.debug('> hap update', settings.name, 'Active', active);
+                valve.getService(Service.Valve)
+                    .updateCharacteristic(Characteristic.Active, active);
+            });
+            valve.getService(Service.Valve)
+                .getCharacteristic(Characteristic.Active)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'Active');
+                    const active = mqttStatus[settings.topic.statusActive] === settings.payload.activeTrue ? 1 : 0;
+                    log.debug('> hap re_get', settings.name, 'Active', active);
+                    callback(null, active);
+                });
+        }
+
+
+        mqttSub(settings.topic.statusInUse, val => {
+            log.debug('< mqtt', settings.topic.statusInUse, val);
+            const inUse = mqttStatus[settings.topic.statusInUse] === settings.payload.inUseTrue ? 1 : 0;
+            log.debug('> hap update', settings.name, 'InUse', inUse);
+            valve.getService(Service.Valve)
+                .updateCharacteristic(Characteristic.InUse, inUse);
+        });
+        valve.getService(Service.Valve)
+            .getCharacteristic(Characteristic.InUse)
+            .on('get', callback => {
+                log.debug('< hap get', settings.name, 'InUse');
+                const inUse = mqttStatus[settings.topic.statusInUse] === settings.payload.inUseTrue ? 1 : 0;
+                log.debug('> hap re_get', settings.name, 'InUse', inUse);
+                callback(null, inUse);
+            });
+
+
+
+        if (settings.topic.setDuration) {
+            valve.getService(Service.Valve, settings.name)
+                .getCharacteristic(Characteristic.SetDuration)
+                .on('set', (value, callback) => {
+                    log.debug('< hap set', settings.name, 'SetDuration', value);
+                    log.debug('> mqtt', settings.topic.setDuration, value);
+                    mqttPub(settings.topic.setDuration, value);
+                    callback();
+                });
+        }
+
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusRemainingDuration) {
+            mqttSub(settings.topic.statusRemainingDuration, val => {
+                log.debug('< mqtt', settings.topic.statusRemainingDuration, val);
+                const remainingDuration = mqttStatus[settings.topic.statusRemainingDuration];
+                log.debug('> hap update', settings.name, 'RemainingDuration', remainingDuration);
+                valve.getService(Service.Valve)
+                    .updateCharacteristic(Characteristic.RemainingDuration, remainingDuration);
+            });
+            valve.getService(Service.Valve)
+                .getCharacteristic(Characteristic.RemainingDuration)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'RemainingDuration');
+                    const inUse = mqttStatus[settings.topic.statusRemainingDuration];
+                    log.debug('> hap re_get', settings.name, 'RemainingDuration', inUse);
+                    callback(null, inUse);
+                });
+        }
+
+        /* istanbul ignore else  */
+        if (settings.topic.statusFault) {
+            mqttSub(settings.topic.statusFault, val => {
+                log.debug('< mqtt', settings.topic.statusFault, val);
+                const fault = mqttStatus[settings.topic.statusFault] === settings.payload.faultTrue ? 1 : 0;
+                log.debug('> hap update', settings.name, 'StatusFault', fault);
+                valve.getService(Service.Valve)
+                    .updateCharacteristic(Characteristic.StatusFault, fault);
+            });
+            valve.getService(Service.Valve)
+                .getCharacteristic(Characteristic.StatusFault)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'StatusFault');
+                    const fault = mqttStatus[settings.topic.statusFault] === settings.payload.faultTrue ? 1 : 0;
+                    log.debug('> hap re_get', settings.name, 'StatusFault', fault);
+                    callback(null, fault);
+                });
+        }
+
+        return valve;
+    }
 };
