bd04450a8b5df63ccc6225143f37a4bb323c97bf
m17design <558758+m17design@users.noreply.github.com>
2018-06-30 03:11:09 +0800

    Update CarbonDioxideSensor.js
    add CarbonDioxideLevel and CarbonDioxidePeakLevel

--- services/CarbonDioxideSensor.js
+++ services/CarbonDioxideSensor.js
@@ -115,6 +115,39 @@ module.exports = function (iface) {
                     .updateCharacteristic(Characteristic.StatusTampered, tampered);
             });
         }
+        
+        if (settings.topic.statusCarbonDioxideLevel) {
+             mqttSub(settings.topic.statusCarbonDioxideLevel, val => {
+                log.debug('> hap update', settings.name, 'CarbonDioxideLevel', val);
+                acc.getService(subtype)
+                    .updateCharacteristic(Characteristic.CarbonDioxideLevel, val);
+            });
+            acc.getService(subtype)
+                .getCharacteristic(Characteristic.CarbonDioxideLevel)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'CarbonDioxideLevel');
+                    log.debug('> hap re_get', settings.name, 'CarbonDioxideLevel', mqttStatus[settings.topic.statusCarbonDioxideLevel]);
+                    callback(null, mqttStatus[settings.topic.statusCarbonDioxideLevel]);
+                });       	
+      	}
+      	
+        if (settings.topic.statusCarbonDioxidePeakLevel) {
+              mqttSub(settings.topic.statusCarbonDioxidePeakLevel, val => {
+                log.debug('> hap update', settings.name, 'CarbonDioxidePeakLevel', val);
+                acc.getService(subtype)
+                    .updateCharacteristic(Characteristic.CarbonDioxidePeakLevel, val);
+            });
+            acc.getService(subtype)
+                .getCharacteristic(Characteristic.CarbonDioxidePeakLevel)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'CarbonDioxidePeakLevel');
+                    log.debug('> hap re_get', settings.name, 'CarbonDioxidePeakLevel', mqttStatus[settings.topic.statusCarbonDioxidePeakLevel]);
+                    callback(null, mqttStatus[settings.topic.statusCarbonDioxidePeakLevel]);
+                });       	
+       	
+      	}
+
+
     };
 };
 
