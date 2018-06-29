ec034e6c9bc58e583883b670c701c73df22b1b6a
hobbyquaker <bastiraff@googlemail.com>
2018-06-29 22:49:24 +0200

    fix lint issues

--- services/AirQualitySensor.js
+++ services/AirQualitySensor.js
@@ -52,17 +52,17 @@ module.exports = function (iface) {
                 .getCharacteristic(Characteristic.StatusLowBattery)
                 .on('get', callback => {
                     log.debug('< hap get', settings.name, 'StatusLowBattery');
-                    const bat = mqttStatus[settings.topic.statusLowBattery] !== settings.payload.onLowBattery ?
-                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
-                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+                    const bat = mqttStatus[settings.topic.statusLowBattery] === settings.payload.onLowBattery ?
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                        Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
                     log.debug('> hap re_get', settings.name, 'StatusLowBattery', bat);
                     callback(null, bat);
                 });
 
             mqttSub(settings.topic.statusLowBattery, val => {
-                const bat = val !== settings.payload.onLowBattery ?
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL :
-                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
+                const bat = val === settings.payload.onLowBattery ?
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
+                    Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
                 log.debug('> hap update', settings.name, 'StatusLowBattery', bat);
                 acc.getService(subtype)
                     .updateCharacteristic(Characteristic.StatusLowBattery, bat);
@@ -96,17 +96,17 @@ module.exports = function (iface) {
                 .getCharacteristic(Characteristic.StatusFault)
                 .on('get', callback => {
                     log.debug('< hap get', settings.name, 'StatusFault');
-                    const fault = mqttStatus[settings.topic.statusFault] !== settings.payload.onFault ?
-                        Characteristic.StatusFault.NO_FAULT :
-                        Characteristic.StatusFault.GENERAL_FAULT;
+                    const fault = mqttStatus[settings.topic.statusFault] === settings.payload.onFault ?
+                        Characteristic.StatusFault.GENERAL_FAULT :
+                        Characteristic.StatusFault.NO_FAULT;
                     log.debug('> hap re_get', settings.name, 'StatusFault', fault);
                     callback(null, fault);
                 });
 
             mqttSub(settings.topic.statusFault, val => {
-                const fault = val !== settings.payload.onFault ?
-                    Characteristic.StatusFault.NO_FAULT :
-                    Characteristic.StatusFault.GENERAL_FAULT;
+                const fault = val === settings.payload.onFault ?
+                    Characteristic.StatusFault.GENERAL_FAULT :
+                    Characteristic.StatusFault.NO_FAULT;
                 log.debug('> hap update', settings.name, 'StatusFault', fault);
                 acc.getService(subtype)
                     .updateCharacteristic(Characteristic.StatusFault, fault);
@@ -120,17 +120,17 @@ module.exports = function (iface) {
                 .getCharacteristic(Characteristic.StatusTampered)
                 .on('get', callback => {
                     log.debug('< hap get', settings.name, 'StatusTampered');
-                    const tampered = mqttStatus[settings.topic.statusTampered] !== settings.payload.onTampered ?
-                        Characteristic.StatusTampered.NOT_TAMPERED :
-                        Characteristic.StatusTampered.TAMPERED;
+                    const tampered = mqttStatus[settings.topic.statusTampered] === settings.payload.onTampered ?
+                        Characteristic.StatusTampered.TAMPERED :
+                        Characteristic.StatusTampered.NOT_TAMPERED;
                     log.debug('> hap re_get', settings.name, 'StatusTampered', tampered);
                     callback(null, tampered);
                 });
 
             mqttSub(settings.topic.statusTampered, val => {
-                const tampered = val !== settings.payload.onTampered ?
-                    Characteristic.StatusTampered.NOT_TAMPERED :
-                    Characteristic.StatusTampered.TAMPERED;
+                const tampered = val === settings.payload.onTampered ?
+                    Characteristic.StatusTampered.TAMPERED :
+                    Characteristic.StatusTampered.NOT_TAMPERED;
                 log.debug('> hap update', settings.name, 'StatusTampered', tampered);
                 acc.getService(subtype)
                     .updateCharacteristic(Characteristic.StatusTampered, tampered);
