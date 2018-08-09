e3ac7c3bdb101f2eebd37d77dc6a79515234b5f4
hobbyquaker <bastiraff@googlemail.com>
2018-08-09 19:11:15 +0200

    cleanup and document SecuritySystem

--- services/SecuritySystem.js
+++ services/SecuritySystem.js
@@ -1,67 +1,20 @@
 /* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off" */
 
 module.exports = function (iface) {
-    // TODO
 
     const {mqttPub, mqttSub, mqttStatus, log, Service, Characteristic} = iface;
 
     return function createService_SecuritySystem(acc, settings, subtype) {
-        // Required Characteristics
-
-        /*
-         this.addCharacteristic(Characteristic.SecuritySystemTargetState);
-
-         Characteristic.SecuritySystemTargetState.STAY_ARM = 0;
-         Characteristic.SecuritySystemTargetState.AWAY_ARM = 1;
-         Characteristic.SecuritySystemTargetState.NIGHT_ARM = 2;
-         Characteristic.SecuritySystemTargetState.DISARM = 3;
-
-         */
 
         acc.addService(Service.SecuritySystem, settings.name, subtype)
             .getCharacteristic(Characteristic.SecuritySystemTargetState)
             .on('set', (value, callback) => {
                 log.debug('< hap set', settings.name, 'SecuritySystemTargetState', value);
-                /*
-                If (value === Characteristic.SecuritySystemTargetState.STAY_ARM) {
-                    value = settings.payload.STAY_ARM;
-                } else if (value === Characteristic.SecuritySystemTargetState.AWAY_ARM) {
-                    value = settings.payload.AWAY_ARM;
-                } else if (value === Characteristic.SecuritySystemTargetState.NIGHT_ARM) {
-                    value = settings.payload.NIGHT_ARM;
-                } else if (value === Characteristic.SecuritySystemTargetState.DISARM) {
-                    value = settings.payload.DISARMED;
-                }
-                */
-                log.debug('> mqtt', settings.topic.setSecuritySystemTargetState, value);
                 mqttPub(settings.topic.setSecuritySystemTargetState, value);
                 callback();
             });
 
-        /*
-         This.addCharacteristic(Characteristic.SecuritySystemCurrentState);
-
-         Characteristic.SecuritySystemCurrentState.STAY_ARM = 0;
-         Characteristic.SecuritySystemCurrentState.AWAY_ARM = 1;
-         Characteristic.SecuritySystemCurrentState.NIGHT_ARM = 2;
-         Characteristic.SecuritySystemCurrentState.DISARMED = 3;
-         Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED = 4;
-         */
-
         mqttSub(settings.topic.statusSecuritySystemCurrentState, val => {
-            /*
-            If (val === settings.payload.STAY_ARM) {
-                val = Characteristic.SecuritySystemCurrentState.STAY_ARM;
-            } else if (val === settings.payload.AWAY_ARM) {
-                val = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-            } else if (val === settings.payload.NIGHT_ARM) {
-                val = Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
-            } else if (val === settings.payload.DISARMED) {
-                val = Characteristic.SecuritySystemCurrentState.DISARMED;
-            } else if (val === settings.payload.ALARM_TRIGGERED) {
-                val = Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED;
-            }
-            */
             log.debug('> hap update', settings.name, 'SecuritySystemCurrentState', val);
             acc.getService(subtype)
                 .setCharacteristic(Characteristic.SecuritySystemCurrentState, val);
@@ -76,19 +29,6 @@ module.exports = function (iface) {
             .on('get', callback => {
                 log.debug('< hap get', settings.name, 'SecuritySystemCurrentState');
                 const val = mqttStatus[settings.topic.statusSecuritySystemCurrentState];
-                /*
-                If (val === settings.payload.STAY_ARM) {
-                    val = Characteristic.SecuritySystemCurrentState.STAY_ARM;
-                } else if (val === settings.payload.AWAY_ARM) {
-                    val = Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-                } else if (val === settings.payload.NIGHT_ARM) {
-                    val = Characteristic.SecuritySystemCurrentState.NIGHT_ARM;
-                } else if (val === settings.payload.DISARMED) {
-                    val = Characteristic.SecuritySystemCurrentState.DISARMED;
-                } else if (val === settings.payload.ALARM_TRIGGERED) {
-                    val = Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED;
-                }
-                */
                 log.debug('> hap re_get', settings.name, 'SecuritySystemCurrentState', val);
                 callback(null, val);
             });
