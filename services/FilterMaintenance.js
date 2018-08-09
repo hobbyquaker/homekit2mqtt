21904e17a185d2e752b5442ed9ecb998760a45f3
hobbyquaker <bastiraff@googlemail.com>
2018-08-09 19:59:37 +0200

    implement Service FilterMaintenance (closes #71)

--- services/FilterMaintenance.js
+++ services/FilterMaintenance.js
@@ -3,19 +3,48 @@
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, Service, Characteristic} = iface;
 
-    // TODO Implement
-
-    /*
-  // Required Characteristics
-  this.addCharacteristic(Characteristic.FilterChangeIndication);
-
-  // Optional Characteristics
-  this.addOptionalCharacteristic(Characteristic.FilterLifeLevel);
-  this.addOptionalCharacteristic(Characteristic.ResetFilterIndication);
-  this.addOptionalCharacteristic(Characteristic.Name);
-     */
-
     return function createService_FilterMaintenance(acc, settings, subtype) {
-        throw new Error('Service FilterMaintenance not yet implemented');
+        acc.addService(Service.FilterMaintenance, settings.name, subtype)
+            .getCharacteristic(Characteristic.FilterChangeIndication)
+            .on('get', callback => {
+                const val = mqttStatus[settings.topic.statusFilterChangeIndication];
+                log.debug('< hap get', settings.name, 'FilterChangeIndication');
+                log.debug('> hap re_get', settings.name, val);
+                callback(null, val);
+            });
+
+        mqttSub(settings.topic.statusFilterChangeIndication, val => {
+            log.debug('> hap update', settings.name, 'FilterChangeIndication', val);
+            acc.getService(subtype)
+                .updateCharacteristic(Characteristic.FilterChangeIndication, val);
+        });
+
+        if (settings.topic.statusFilterLifeLevel) {
+            acc.getService(subtype)
+                .getCharacteristic(Characteristic.FilterLifeLevel)
+                .on('get', callback => {
+                    log.debug('< hap get', settings.name, 'FilterLifeLevel');
+                    const level = mqttStatus[settings.topic.statusFilterLifeLevel];
+
+                    log.debug('> hap re_get', settings.name, 'FilterLifeLevel', level);
+                    callback(null, level);
+                });
+
+            mqttSub(settings.topic.statusFilterLifeLevel, val => {
+                log.debug('> hap update', settings.name, 'FilterLifeLevel', val);
+                acc.getService(subtype)
+                    .updateCharacteristic(Characteristic.FilterLifeLevel, val);
+            });
+        }
+
+        if (settings.topic.setResetFilterIndication) {
+            acc.getService(subtype)
+                .getCharacteristic(Characteristic.ResetFilterIndication)
+                .on('set', (value, callback) => {
+                    log.debug('< hap set', settings.name, 'ResetFilterIndication', value);
+                    mqttPub(settings.topic.setResetFilterIndication, value);
+                    callback();
+                });
+        }
     };
 };
