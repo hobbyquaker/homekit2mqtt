c5e37ca84a8ab241c53f17510ced02df972b2acb
hobbyquaker <bastiraff@googlemail.com>
2018-08-18 18:30:04 +0200

    fix CurrentRelativeHumidity topic confusion (#120)

--- characteristics/CurrentRelativeHumidity.js
+++ characteristics/CurrentRelativeHumidity.js
@@ -4,17 +4,21 @@ module.exports = function (obj, iface) {
     const {acc, settings, subtype} = obj;
     const {mqttStatus, mqttPub, mqttSub, Characteristic, log} = iface;
 
-    acc.getService(subtype)
-        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        .on('get', callback => {
-            log.debug('< hap get', settings.name, 'CurrentRelativeHumidity');
-            log.debug('> hap re_get', settings.name, mqttStatus[settings.topic.statusHumidity]);
-            callback(null, mqttStatus[settings.topic.statusHumidity]);
-        });
+    settings.topic.statusCurrentRelativeHumidity = settings.topic.statusCurrentRelativeHumidity || settings.topic.statusHumidity;
 
-    mqttSub(settings.topic.statusHumidity, val => {
-        log.debug('> hap update', settings.name, 'CurrentRelativeHumidity', mqttStatus[settings.topic.statusHumidity]);
+    /* istanbul ignore else */
+    if (settings.topic.statusCurrentRelativeHumidity) {
+        mqttSub(settings.topic.statusCurrentRelativeHumidity, val => {
+            log.debug('> hap update', settings.name, 'CurrentRelativeHumidity', val);
+            acc.getService(subtype)
+                .updateCharacteristic(Characteristic.CurrentRelativeHumidity, val);
+        });
         acc.getService(subtype)
-            .updateCharacteristic(Characteristic.CurrentRelativeHumidity, val);
-    });
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .on('get', callback => {
+                log.debug('< hap get', settings.name, 'CurrentRelativeHumidity');
+                log.debug('> hap re_get', settings.name, 'CurrentRelativeHumidity', mqttStatus[settings.topic.statusCurrentRelativeHumidity]);
+                callback(null, mqttStatus[settings.topic.statusCurrentRelativeHumidity]);
+            });
+    }
 };
