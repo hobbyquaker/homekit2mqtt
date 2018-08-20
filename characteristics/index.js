73dfbaf18a73db0d0bf1947d2ddfb0a7e4c25186
hobbyquaker <bastiraff@googlemail.com>
2018-08-20 23:44:45 +0200

    refactoring, setProps

--- characteristics/index.js
+++ characteristics/index.js
@@ -1,44 +1,44 @@
 /* eslint unicorn/filename-case: "off", func-names: "off", camelcase: "off", no-unused-vars: "off" */
 
-module.exports = function (characteristic, obj, iface) {
+module.exports = function (charName, obj, iface) {
     const {acc, settings, subtype} = obj;
     const {mqttStatus, mqttPub, mqttSub, Characteristic, log} = iface;
 
-    if (!Characteristic[characteristic]) {
-        throw new Error('Unknown Characteristic ' + characteristic);
+    if (!Characteristic[charName]) {
+        throw new Error('Unknown Characteristic ' + charName);
     }
 
-    const setTopic = 'set' + characteristic;
-    const statusTopic = 'status' + characteristic;
+    const setTopic = 'set' + charName;
+    const statusTopic = 'status' + charName;
 
-    const {props, eventOnlyCharacteristic} = acc.getService(subtype).getCharacteristic(Characteristic[characteristic]);
+    const service = acc.getService(subtype);
+    const characteristic = service.getCharacteristic(Characteristic[charName]);
 
-    console.log('props', props);
+    if (settings.props && settings.props[charName]) {
+        characteristic.setProps(settings.props[charName]);
+    }
+
+    const {props, eventOnlyCharacteristic} = characteristic;
 
     /* istanbul ignore else */
     if (props.perms.includes(Characteristic.Perms.PAIRED_READ) && settings.topic[statusTopic]) {
         mqttSub(settings.topic[statusTopic], val => {
-            log.debug('> hap update', settings.name, characteristic, val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic[characteristic], val);
+            log.debug('> hap update', settings.name, charName, val);
+            service.updateCharacteristic(Characteristic[charName], val);
         });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic[characteristic])
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, characteristic);
-                log.debug('> hap re_get', settings.name, characteristic, mqttStatus[settings.topic[statusTopic]]);
-                callback(null, mqttStatus[settings.topic[statusTopic]]);
-            });
+        characteristic.on('get', callback => {
+            log.debug('< hap get', settings.name, charName);
+            log.debug('> hap re_get', settings.name, charName, mqttStatus[settings.topic[statusTopic]]);
+            callback(null, mqttStatus[settings.topic[statusTopic]]);
+        });
     }
 
     /* istanbul ignore else */
     if (props.perms.includes(Characteristic.Perms.PAIRED_WRITE) && settings.topic[setTopic]) {
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic[characteristic])
-            .on('set', (value, callback) => {
-                log.debug('< hap set', settings.name, characteristic, value);
-                mqttPub(settings.topic[setTopic], value);
-                callback();
-            });
+        characteristic.on('set', (value, callback) => {
+            log.debug('< hap set', settings.name, charName, value);
+            mqttPub(settings.topic[setTopic], value);
+            callback();
+        });
     }
 };
