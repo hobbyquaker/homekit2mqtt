3634ee6ae8bc8bbfc51027fc7734570c3bbda5b1
hobbyquaker <bastiraff@googlemail.com>
2018-08-21 16:20:47 +0200

    refactor Fanv2 (#126)

--- services/Fanv2.js
+++ services/Fanv2.js
@@ -6,46 +6,15 @@ module.exports = function (iface) {
     return function createService_Fanv2(acc, settings, subtype) {
         acc.addService(Service.Fanv2);
 
-        mqttSub(settings.topic.statusCurrentFanState, val => {
-            log.debug('> hap update', settings.name, 'CurrentFanState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.CurrentFanState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.CurrentFanState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'CurrentFanState');
-                const state = mqttStatus[settings.topic.statusCurrentFanState];
-                log.debug('> hap re_get', settings.name, 'CurrentFanState', state);
-                callback(null, state);
-            });
+        const obj = {acc, settings, subtype};
 
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetFanState)
-            .on('set', (value, callback) => {
-                log.debug('< hap set', settings.name, 'TargetFanState', value);
-                mqttPub(settings.topic.setTargetFanState, value);
-                callback();
-            });
+        require('../characteristics')('CurrentFanState', obj, iface);
+        require('../characteristics')('TargetFanState', obj, iface);
 
-        mqttSub(settings.topic.statusTargetFanState, val => {
-            log.debug('> hap update', settings.name, 'TargetFanState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.TargetFanState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetFanState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'TargetFanState');
-                const state = mqttStatus[settings.topic.statusTargetFanState];
-                log.debug('> hap re_get', settings.name, 'TargetFanState', state);
-                callback(null, state);
-            });
-
-        require('../characteristics/Active')({acc, settings, subtype}, iface);
-        require('../characteristics/RotationSpeed')({acc, settings, subtype}, iface);
-        require('../characteristics/RotationDirection')({acc, settings, subtype}, iface);
-        require('../characteristics/LockPhysicalControls')({acc, settings, subtype}, iface);
-        require('../characteristics/SwingMode')({acc, settings, subtype}, iface);
+        require('../characteristics/Active')(obj, iface);
+        require('../characteristics/RotationSpeed')(obj, iface);
+        require('../characteristics/RotationDirection')(obj, iface);
+        require('../characteristics/LockPhysicalControls')(obj, iface);
+        require('../characteristics/SwingMode')(obj, iface);
     };
 };
