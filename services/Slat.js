4f2c1d8331edea93620da6bf5550043f293164e7
hobbyquaker <bastiraff@googlemail.com>
2018-08-21 17:41:04 +0200

    refactor Slat (#126)

--- services/Slat.js
+++ services/Slat.js
@@ -3,68 +3,21 @@
 module.exports = function (iface) {
     const {mqttPub, mqttSub, mqttStatus, log, Service, Characteristic} = iface;
 
-    /*
-    // Required Characteristics
-    this.addCharacteristic(Characteristic.SlatType);
-    this.addCharacteristic(Characteristic.CurrentSlatState);
-
-    // The value property of CurrentSlatState must be one of the following:
-    Characteristic.CurrentSlatState.FIXED = 0;
-    Characteristic.CurrentSlatState.JAMMED = 1;
-    Characteristic.CurrentSlatState.SWINGING = 2;
-
-    // Optional Characteristics
-    this.addOptionalCharacteristic(Characteristic.Name);
-    this.addOptionalCharacteristic(Characteristic.CurrentTiltAngle);
-    this.addOptionalCharacteristic(Characteristic.TargetTiltAngle);
-    this.addOptionalCharacteristic(Characteristic.SwingMode);
-
-    // The value property of SwingMode must be one of the following:
-    Characteristic.SwingMode.SWING_DISABLED = 0;
-    Characteristic.SwingMode.SWING_ENABLED = 1;
-     */
-
     return function createService_Slat(acc, settings, subtype) {
         acc.addService(Service.Slat, settings.name, subtype);
 
-        mqttSub(settings.topic.statusCurrentSlatState, val => {
-            const angle = mqttStatus[settings.topic.statusCurrentSlatState];
-            log.debug('> hap update', settings.name, 'CurrentSlatState', angle);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.CurrentSlatState, angle);
-        });
-
         const type = settings.config.SlatType || 0;
+
         log.debug('> hap set', settings.name, 'SlatType', type);
         acc.getService(subtype)
             .setCharacteristic(Characteristic.SlatType, type);
 
-        if (settings.topic.statusCurrentTiltAngle) {
-            mqttSub(settings.topic.statusCurrentTiltAngle, val => {
-                log.debug('> hap update', settings.name, 'CurrentTiltAngle', val);
-                acc.getService(subtype)
-                    .updateCharacteristic(Characteristic.CurrentTiltAngle, val);
-            });
-        }
+        const obj = {acc, settings, subtype};
 
-        if (settings.topic.statusTargetTiltAngle) {
-            mqttSub(settings.topic.statusTargetTiltAngle, val => {
-                log.debug('> hap update', settings.name, 'TargetTiltAngle', val);
-                acc.getService(subtype)
-                    .updateCharacteristic(Characteristic.TargetTiltAngle, val);
-            });
-        }
+        require('../characteristics')('TargetTiltAngle', obj, iface);
+        require('../characteristics')('CurrentTiltAngle', obj, iface);
+        require('../characteristics')('CurrentSlatState', obj, iface);
 
-        if (settings.topic.setTargetTiltAngle) {
-            acc.getService(subtype)
-                .getCharacteristic(Characteristic.TargetTiltAngle)
-                .on('set', (value, callback) => {
-                    log.debug('< hap set', settings.name, 'TargetTiltAngle', value);
-                    mqttPub(settings.topic.setTargetTiltAngle, value);
-                    callback();
-                });
-        }
-
-        require('../characteristics/SwingMode')({acc, settings, subtype}, iface);
+        require('../characteristics/SwingMode')(obj, iface);
     };
 };
