71476f7a547a24ccd2650bb161820ebb6c0b44f9
hobbyquaker <bastiraff@googlemail.com>
2018-08-21 16:18:50 +0200

    refactor AirPurifier (#126)

--- services/AirPurifier.js
+++ services/AirPurifier.js
@@ -12,45 +12,14 @@ module.exports = function (iface) {
             settings.payload.activeFalse = false;
         }
 
-        mqttSub(settings.topic.statusCurrentAirPurifierState, val => {
-            log.debug('> hap update', settings.name, 'CurrentAirPurifierState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.CurrentAirPurifierState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.CurrentAirPurifierState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'CurrentAirPurifierState');
-                const state = mqttStatus[settings.topic.statusCurrentAirPurifierState];
-                log.debug('> hap re_get', settings.name, 'CurrentAirPurifierState', state);
-                callback(null, state);
-            });
+        const obj = {acc, settings, subtype};
 
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetAirPurifierState)
-            .on('set', (value, callback) => {
-                log.debug('< hap set', settings.name, 'TargetAirPurifierState', value);
-                mqttPub(settings.topic.setTargetAirPurifierState, value);
-                callback();
-            });
+        require('../characteristics')('CurrentAirPurifierState', obj, iface);
+        require('../characteristics')('TargetAirPurifierState', obj, iface);
 
-        mqttSub(settings.topic.statusTargetAirPurifierState, val => {
-            log.debug('> hap update', settings.name, 'TargetAirPurifierState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.TargetAirPurifierState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetAirPurifierState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'TargetAirPurifierState');
-                const state = mqttStatus[settings.topic.statusTargetAirPurifierState];
-                log.debug('> hap re_get', settings.name, 'TargetAirPurifierState', state);
-                callback(null, state);
-            });
-
-        require('../characteristics/Active')({acc, settings, subtype}, iface);
-        require('../characteristics/RotationSpeed')({acc, settings, subtype}, iface);
-        require('../characteristics/SwingMode')({acc, settings, subtype}, iface);
-        require('../characteristics/LockPhysicalControls')({acc, settings, subtype}, iface);
+        require('../characteristics/Active')(obj, iface);
+        require('../characteristics/RotationSpeed')(obj, iface);
+        require('../characteristics/SwingMode')(obj, iface);
+        require('../characteristics/LockPhysicalControls')(obj, iface);
     };
 };
