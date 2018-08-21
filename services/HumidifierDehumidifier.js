19a6dafdc5a4ff5cf4216b9f565758259beb9350
hobbyquaker <bastiraff@googlemail.com>
2018-08-21 16:35:46 +0200

    refactor HumidifierDehumidifier (#126)

--- services/HumidifierDehumidifier.js
+++ services/HumidifierDehumidifier.js
@@ -14,123 +14,16 @@ module.exports = function (iface) {
 
         acc.addService(Service.HumidifierDehumidifier, settings.name, subtype);
 
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.WaterLevel)
-            .setProps((settings.props || {}).WaterLevel)
-            .on('get', callback => {
-                const humidity = mqttStatus[settings.topic.statusWaterLevel];
-                log.debug('< hap get', settings.name, 'WaterLevel');
-                log.debug('> hap re_get', settings.name, humidity);
-                callback(null, humidity);
-            });
-
-        mqttSub(settings.topic.statusWaterLevel, val => {
-            log.debug('> hap update', settings.name, 'WaterLevel', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.WaterLevel, val);
-        });
-
-        mqttSub(settings.topic.statusCurrentHumidifierDehumidifierState, val => {
-            log.debug('> hap update', settings.name, 'CurrentHumidifierDehumidifierState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.CurrentHumidifierDehumidifierState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.CurrentHumidifierDehumidifierState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'CurrentHumidifierDehumidifierState');
-                const state = mqttStatus[settings.topic.statusCurrentHumidifierDehumidifierState];
-                log.debug('> hap re_get', settings.name, 'CurrentHumidifierDehumidifierState', state);
-                callback(null, state);
-            });
-
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetHumidifierDehumidifierState)
-            .on('set', (value, callback) => {
-                log.debug('< hap set', settings.name, 'TargetHumidifierDehumidifierState', value);
-                mqttPub(settings.topic.setTargetHumidifierDehumidifierState, value);
-                callback();
-            });
-
-        mqttSub(settings.topic.statusTargetHumidifierDehumidifierState, val => {
-            log.debug('> hap update', settings.name, 'TargetHumidifierDehumidifierState', val);
-            acc.getService(subtype)
-                .updateCharacteristic(Characteristic.TargetHumidifierDehumidifierState, val);
-        });
-        acc.getService(subtype)
-            .getCharacteristic(Characteristic.TargetHumidifierDehumidifierState)
-            .on('get', callback => {
-                log.debug('< hap get', settings.name, 'TargetHumidifierDehumidifierState');
-                const state = mqttStatus[settings.topic.statusTargetHumidifierDehumidifierState];
-                log.debug('> hap re_get', settings.name, 'TargetHumidifierDehumidifierState', state);
-                callback(null, state);
-            });
-
-        /* istanbul ignore else */
-        if (settings.topic.setRelativeHumidityDehumidifierThreshold) {
-            acc.getService(subtype)
-                .getCharacteristic(Characteristic.RelativeHumidityDehumidifierThreshold)
-                .on('set', (value, callback) => {
-                    log.debug('< hap set', settings.name, 'RelativeHumidityDehumidifierThreshold', value);
-                    mqttPub(settings.topic.setRelativeHumidityDehumidifierThreshold, value, settings.mqttPublishOptions);
-                    callback();
-                });
-        }
-
-        /* istanbul ignore else */
-        if (settings.topic.statusRelativeHumidityDehumidifierThreshold) {
-            mqttSub(settings.topic.statusRelativeHumidityDehumidifierThreshold, val => {
-                log.debug('> hap update', settings.name, 'RelativeHumidityDehumidifierThreshold', val);
-                acc.getService(subtype)
-                    .updateCharacteristic(Characteristic.RelativeHumidityDehumidifierThreshold, val);
-            });
-
-            acc.getService(subtype)
-                .getCharacteristic(Characteristic.RelativeHumidityDehumidifierThreshold)
-                .on('get', callback => {
-                    log.debug('< hap get', settings.name, 'RelativeHumidityDehumidifierThreshold');
-                    log.debug('> hap re_get', settings.name, 'RelativeHumidityDehumidifierThreshold', mqttStatus[settings.topic.statusRelativeHumidityDehumidifierThreshold]);
-                    callback(null, mqttStatus[settings.topic.statusRelativeHumidityDehumidifierThreshold]);
-                });
-        }
-
-        /* istanbul ignore else */
-        if (settings.topic.setRelativeHumidityHumidifierThreshold) {
-            acc.getService(subtype)
-                .getCharacteristic(Characteristic.RelativeHumidityHumidifierThreshold)
-                .on('set', (value, callback) => {
-                    log.debug('< hap set', settings.name, 'RelativeHumidityHumidifierThreshold', value);
-                    mqttPub(settings.topic.setRelativeHumidityHumidifierThreshold, value, settings.mqttPublishOptions);
-                    callback();
-                });
-        }
-
-        /* istanbul ignore else */
-        if (settings.topic.statusRelativeHumidityHumidifierThreshold) {
-            mqttSub(settings.topic.statusRelativeHumidityHumidifierThreshold, val => {
-                log.debug('> hap update', settings.name, 'RelativeHumidityHumidifierThreshold', val);
-                acc.getService(subtype)
-                    .updateCharacteristic(Characteristic.RelativeHumidityHumidifierThreshold, val);
-            });
-
-            acc.getService(subtype)
-                .getCharacteristic(Characteristic.RelativeHumidityHumidifierThreshold)
-                .on('get', callback => {
-                    log.debug('< hap get', settings.name, 'RelativeHumidityHumidifierThreshold');
-                    log.debug('> hap re_get', settings.name, 'RelativeHumidityHumidifierThreshold', mqttStatus[settings.topic.statusRelativeHumidityHumidifierThreshold]);
-                    callback(null, mqttStatus[settings.topic.statusRelativeHumidityHumidifierThreshold]);
-                });
-        }
-
         /* istanbul ignore else */
         if (settings.topic.statusWaterLevel) {
             acc.getService(subtype)
                 .getCharacteristic(Characteristic.WaterLevel)
+                .setProps((settings.props || {}).WaterLevel)
                 .on('get', callback => {
-                    const humidity = mqttStatus[settings.topic.statusWaterLevel] / (settings.payload.waterLevelFactor || 1);
+                    const water = mqttStatus[settings.topic.statusWaterLevel] / (settings.payload.waterLevelFactor || 1);
                     log.debug('< hap get', settings.name, 'WaterLevel');
-                    log.debug('> hap re_get', settings.name, humidity);
-                    callback(null, humidity);
+                    log.debug('> hap re_get', settings.name, water);
+                    callback(null, water);
                 });
 
             mqttSub(settings.topic.statusWaterLevel, val => {
@@ -141,8 +34,15 @@ module.exports = function (iface) {
             });
         }
 
-        require('../characteristics/Active')({acc, settings, subtype}, iface);
-        require('../characteristics/RotationSpeed')({acc, settings, subtype}, iface);
-        require('../characteristics/SwingMode')({acc, settings, subtype}, iface);
+        const obj = {acc, settings, subtype};
+
+        require('../characteristics')('CurrentHumidifierDehumidifierState', obj, iface);
+        require('../characteristics')('TargetHumidifierDehumidifierState', obj, iface);
+        require('../characteristics')('RelativeHumidityDehumidifierThreshold', obj, iface);
+        require('../characteristics')('RelativeHumidityHumidifierThreshold', obj, iface);
+
+        require('../characteristics/Active')(obj, iface);
+        require('../characteristics/RotationSpeed')(obj, iface);
+        require('../characteristics/SwingMode')(obj, iface);
     };
 };
